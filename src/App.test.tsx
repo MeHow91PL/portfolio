3eba3ac00c2ab127ab267c275d500c0de2798863
Michał Kokosiński <19owen91@gmail.com>
2018-08-07 10:59:48 +0200

    resolved conflicts

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-=======
 // import * as React from 'react';
 // import * as ReactDOM from 'react-dom';
 // import App from './App';
@@ -18,4 +7,3 @@ it('renders without crashing', () => {
 //   ReactDOM.render(<App />, div);
 //   ReactDOM.unmountComponentAtNode(div);
 // });
->>>>>>> cd1034b9c2ceffd335d95c2e25b82dc26e242f07
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(
-  <App />,
-=======
 import registerServiceWorker from './registerServiceWorker';
 
 import {Provider} from 'react-redux';
@@ -25,7 +17,6 @@ ReactDOM.render(
     <App history={history}/>
   </Provider>
   ,
->>>>>>> cd1034b9c2ceffd335d95c2e25b82dc26e242f07
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
